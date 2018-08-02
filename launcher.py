cac56fc0ff298a358b22a38d0ed4e2f10bbd5b2a
Sriee Sathiiss <Sriee_Sathiiss@symantec.com>
2018-08-01 17:46:33 -0700

    add better arguments for client

--- launcher.py
+++ launcher.py
@@ -7,7 +7,6 @@ import json
 import logging.config
 
 from collections import namedtuple
-from random import randint
 
 Specification = namedtuple('Specification', 'pid host port')
 logger = logging.getLogger('main')
@@ -15,38 +14,43 @@ logger = logging.getLogger('main')
 
 class Client(object):
 
-    def __init__(self, num_clients):
+    def __init__(self, num_clients, name, host, port):
         self._num_of_clients = int(num_clients)
         self._clients = {}
-        self._assigned = set()
+        self._name = name
+        self._host = host
+        self._port = port
         self._persistent_file_name = '.clients.db'
 
     @property
     def num_of_clients(self):
         return self._num_of_clients
 
+    @property
+    def host(self):
+        return self._host
+
+    @property
+    def port(self):
+        return self._port
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if self._name is None:
+            self._name = value
+
     def start_client(self):
         for i in range(1, self._num_of_clients + 1):
-            name, host, port = 'Client-{}'.format(i), '127.0.0.1', self.get_random_port()
-            process = subprocess.Popen([sys.executable, 'service/client.py', name, host,
-                                        port])
+            self.name = 'Client-{}'.format(i)
+            process = subprocess.Popen([sys.executable, 'service/client.py', self.name, self.host,
+                                        self.port])
 
-            self._clients[name] = Specification(process.pid, '127.0.0.1', port)
-            logger.info('%s started', name)
-
-    def get_random_port(self):
-        _port = None
-
-        while not _port:
-            _port = randint(1025, 1100)
-
-            if _port in self._assigned:
-                _port = None
-                continue
-            else:
-                self._assigned.add(_port)
-
-        return str(_port)
+            self._clients[self.name] = Specification(process.pid, '127.0.0.1', self.port)
+            logger.info('%s started', self.name)
 
     def load_client(self):
         self._clients.clear()
@@ -74,11 +78,15 @@ class Client(object):
             subprocess.run(['kill', str(self._clients[k].pid)])
             logger.debug('{}: Kill {}'.format(k, self._clients[k].pid))
 
+    def __repr__(self):
+        return 'Client(number_of_clients={}, persistent_file_name={}, ' \
+               'is_persistent_storage_present={})'.\
+            format(self._num_of_clients, self._persistent_file_name,
+                   os.path.isfile(os.path.abspath(
+                       os.path.join('log', self._persistent_file_name))))
+
     def __str__(self):
-        return 'Client(number_of_clients={}, is_persistent_storage_present={})'\
-            .format(self._num_of_clients,
-                    os.path.isfile(os.path.abspath(
-                        os.path.join('log', self._persistent_file_name))))
+        return 'Client(name={}, host={}, port={}'.format(self.name, self.host, self.port)
 
 
 def setup_logging(default_path='log_config.json', default_level=logging.INFO):
@@ -98,8 +106,10 @@ def setup_logging(default_path='log_config.json', default_level=logging.INFO):
 
 
 def main(args):
-    peers = Client(args.n)
+    peers = Client(num_clients=args.n, name=args.name, host=args.host,
+                   port=args.port)
 
+    logger.info(peers)
     if args.action == 'start':
         peers.start_client()
 
@@ -111,6 +121,7 @@ def main(args):
         peers.dump_client()
     else:
         peers.stop_client()
+        logger.debug('Stopped clients successfully.')
 
 
 if __name__ == '__main__':
@@ -119,7 +130,10 @@ if __name__ == '__main__':
 
     cli = argparse.ArgumentParser(description='Start / Stop client processes')
     cli.add_argument('-n', help='Number of clients', default=0)
-    cli.add_argument('action', choices=['start', 'stop'], help='Start / Stop clients.')
+    cli.add_argument('--host', help='Host IP of the server', default='127.0.0.1')
+    cli.add_argument('--port', help='Port in which server is listening to', default=1200)
+    cli.add_argument('--name', help='Name of the client')
+    cli.add_argument('action', choices=['start', 'stop'], help='Start / Stop clients')
 
     arg = cli.parse_args()
     logger.debug('Argument Received: %s', arg)
