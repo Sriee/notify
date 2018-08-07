58b75014e401001596e662fb53c33c4c33fcb10c
sriee <psg.sriee@gmail.com>
2018-08-06 22:20:10 -0700

    decouple receiver and state processing
    The issue with this design is that server has to send both the state information as well as the machine.
    Also for each state info received from the server we need a queue as a middle man to fetch and put data. We can avoid this
    extra storage by running listeners for each subscription

--- service/client.py
+++ service/client.py
@@ -1,6 +1,8 @@
 import socket
 import argparse
 from helper import *
+from collections import defaultdict
+from asyncio import Queue
 
 logger = logging.getLogger('main')
 
@@ -12,6 +14,8 @@ class Client(object):
         self._host = host
         self._port = port
         self._subscription = subscription
+        self._listeners = []
+        self._consume_queue = defaultdict(Queue)
 
     @property
     def name(self):
@@ -29,8 +33,31 @@ class Client(object):
     def subscription(self):
         return self._subscription
 
+    @property
+    def num_of_subscription(self):
+        return len(self._subscription)
+
+    async def send_subscriptions(self, writer: StreamWriter):
+        data = ' '.join(self.subscription)
+        data = str(self.num_of_subscription) + ' ' + data
+        writer.write(data.encode())
+        await writer.drain()
+
+    @staticmethod
+    async def receive(sub, queue):
+        logger.info('Receiver listening for \'%s\' state.', sub)
+        while True:
+            msg = queue.get()
+
+            if msg is None:
+                logger.info('Stopping receiver for %', sub)
+                break
+
+            logger.info('Received from server: %s', msg)
+
     async def listener(self, loop):
-        reader, writer = await asyncio.open_connection(host=self.host, port=self.port, loop=loop)
+        reader, writer = await asyncio.open_connection(host=self.host, port=self.port,
+                                                       loop=loop)
         logger.debug('%s @%s', self.name, writer.transport.get_extra_info('sockname'))
 
         try:
@@ -42,13 +69,18 @@ class Client(object):
             message = await read_msg(reader)
 
             if message and message.lower() == 'hello':
-                logger.info('Received reply from server. Sending Subscription...')
-                await send_msg(writer, self.subscription[0])
+                logger.info('Received reply from server. Sending Subscription info...')
+                await self.send_subscriptions(writer)
+
+            # Create separate listeners for each subscription
+            for sub in self.subscription:
+                loop.create_task(self.receive(sub, self._consume_queue[sub]))
 
             while True:
                 data = await read_msg(reader)
                 if data:
-                    logger.info('Received from server: %s', data)
+                    state, machine = data.split(' ')
+                    self._consume_queue[state].put(machine)
 
         except asyncio.CancelledError:
             logger.debug('Stopping Co-routine')
@@ -72,19 +104,24 @@ class Client(object):
             _loop.close()
 
     def __str__(self):
-        return 'Client \'{}\' Status:\nConnected to Server: {}@{}\nSubscriptions: {}'\
+        return 'Client \'{}\' Status:\nConnected to Server: {}@{}\nSubscriptions: {}' \
             .format(self.name, self.host, self.port, ', '.join(self.subscription))
 
 
 if __name__ == '__main__':
     cli = argparse.ArgumentParser(description='''
-                                  Client application to receive machine state notification from the server. 
-                                  Client should subscribe to the required states that it requires the notification
+                                     Client application to receive machine state 
+                                     notification from the server. 
+                                     Client should subscribe to the required states 
+                                     that it requires the 
+                                     notification
                                   ''')
     cli.add_argument('--host', help='Host IP of the server', default='127.0.0.1')
-    cli.add_argument('--port', type=int, help='Port in which server is listening to', default=1200)
+    cli.add_argument('--port', type=int, help='Port in which server is listening to',
+                     default=1200)
     cli.add_argument('--name', help='Name of the client', default=socket.gethostname())
-    cli.add_argument('--sub', choices=['Pending', 'Imaging', 'Executing', 'Error', 'Completed', 'Suspended'],
+    cli.add_argument('--sub', choices=['Pending', 'Imaging', 'Executing', 'Error',
+                                       'Completed', 'Suspended'],
                      nargs='+', default=['Error'], help='Client subscription state')
     args = cli.parse_args()
 
