57bbd4043b6cf75d6c0955a0f604bd79163036b4
Sriee Sathiiss <Sriee_Sathiiss@symantec.com>
2018-08-14 13:07:08 -0700

    sync to async communication trial 1

--- service/test.py
+++ service/test.py
@@ -8,21 +8,24 @@ async def store_it():
     rp = open('temp.txt', 'a')
     try:
         while True:
-            msg = messages.get()
+            msg = await messages.get()
             if not msg:
                 break
             rp.write(msg)
     except asyncio.CancelledError:
         rp.close()
 
+loop = asyncio.get_event_loop()
+loop.run_until_complete(store_it())
 
 if __name__ == '__main__':
-    if sys.argv[1] == 'start':
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(store_it)
-    elif sys.argv[1] == 'stop':
+    if sys.argv[1] == 'stop':
+        print(loop.is_running(), 'stop')
         messages.put(None)
-        loop = asyncio.get_event_loop()
         loop.close()
     elif sys.argv[1] == 'event':
-        messages.put(sys.argv[1] + '\n')
+        print(loop.is_running(), 'event')
+        print(asyncio.Task.all_tasks())
+        messages.put(sys.argv[2] + '\n')
+    else:
+        print('Supported command { stop | event }')
