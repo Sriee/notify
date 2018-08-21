268d36896488de25f96ba4d6afa50d45ebfe9d98
sriee <psg.sriee@gmail.com>
2018-08-20 23:52:19 -0700

    shell script to install mysql udf

--- udf/libudf_sys.sql
+++
@@ -1,3 +0,0 @@
-DROP FUNCTION IF EXISTS sys_call;
-
-CREATE FUNCTION sys_call RETURNS string SONAME 'libudf_sys.so';
--- udf/register_udf.sql
+++ udf/register_udf.sql
@@ -1,7 +1,7 @@
 DROP FUNCTION IF EXISTS sys_call;
 DROP TRIGGER IF EXISTS after_machine_state_update;
 
-CREATE FUNCTION sys_call RETURNS int SONAME 'libudf_sys.so';
+CREATE FUNCTION sys_call RETURNS int SONAME 'libudf.so';
  
 DELIMITER %%
 CREATE TRIGGER after_machine_state_update AFTER UPDATE
@@ -11,7 +11,6 @@ BEGIN
  DECLARE cmd CHAR(255);
  DECLARE res int(10);
  SET cmd = CONCAT('/home/sriee/Git/query/venv/bin/python ', '/home/sriee/Git/query/service/temp.py ', '--state ', NEW.state, ' --machine ', NEW.name);
- INSERT INTO watch(old_state, new_state, py_call) VALUES (OLD.state, NEW.state, cmd);
  SET res = sys_call(cmd);	
 END;
 %%
